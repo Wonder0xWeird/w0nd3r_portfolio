f501485f4441521eb89d6f6445ab1e77f1f14a3a
Lars Crawford <crawford.lars@gmail.com>
2023-09-03 20:22:38 +0700

    add next-optimized-images

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const withPlugins = require("next-compose-plugins")
+const optimizedImages = require("next-optimized-images")
 
-module.exports = nextConfig
+module.exports = withPlugins([
+	[
+		optimizedImages,
+		{
+			/* config for next-optimized-images */
+		},
+	],
+
+	// your other plugins here
+])
