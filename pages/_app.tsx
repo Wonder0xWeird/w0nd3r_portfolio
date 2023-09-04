d87d57fb6a0e4fe1626f3a0efb007bf892cedf4f
Lars Crawford <crawford.lars@gmail.com>
2023-09-04 09:46:14 +0700

    update Head metadata

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -19,7 +19,45 @@ function w0nd3r({ Component, pageProps }: AppProps) {
 				<meta name="title" content="w0nd3r" />
 				<meta
 					name="description"
+					content="A portfolio site for w0nd3r (aka Lars Crawford)."
+				/>
+				{/* Open Graph / Facebook */}
+				<meta property="og:type" content="website" />
+				<meta
+					property="og:url"
+					content="https://w0nd3r.rune.love"
+				/>
+				<meta
+					property="og:title"
 					content="w0nd3r's portfolio website"
+				/>
+				<meta
+					property="og:description"
+					content="A portfolio site for w0nd3r (aka Lars Crawford)."
+				/>
+				<meta
+					property="og:image"
+					content="/images/art/ww_logo.png"
+				/>
+
+				{/* Twitter */}
+				<meta property="twitter:card" content="summary" />
+				<meta
+					property="twitter:url"
+					content="https://w0nd3r.rune.love"
+				/>
+				<meta
+					property="twitter:title"
+					content="w0nd3r's portfolio website"
+				/>
+				<meta
+					property="twitter:description"
+					content="A portfolio site for w0nd3r (aka Lars Crawford)."
+				/>
+				{/* Twitter image must be a absolute url */}
+				<meta
+					property="twitter:image"
+					content="https://w0nd3r.rune.love/images/art/ww_logo.png"
 				/>
 			</Head>
 			<Component {...pageProps} />
